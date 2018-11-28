0268d83a36c61bb05dab846c922cb4b83e558878
Amy Smart <amy.smart@students.mq.edu.au>
2018-11-29 09:34:09 +1100

    load data

--- scripts/workshop.R
+++ scripts/workshop.R
@@ -2,3 +2,12 @@
 ## The csv file containing the data is in the 'data/' directory.
 ## If you want to save plots you created, place them in the 'figures/' directory.
 ## You can create additional R files in the 'scripts/' directory.
+
+library(tidyverse)
+read_csv("data/SAFI_clean.csv")
+interviews<-read_csv("data/SAFI_clean.csv", na="NULL")
+
+interviews
+as.data.frame(interviews)
+View(interviews)
+
