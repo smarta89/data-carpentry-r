ed353e567f50ddf88a3d397fd993f60e0e87f37f
Amy Smart <amy.smart@students.mq.edu.au>
2018-11-29 14:05:02 +1100

    mutate done

--- dplyr.R
+++ dplyr.R
@@ -10,9 +10,19 @@ filter(interviews,1:nrow(interviews)%in%1:10) #same as above
 
 interviews2<-filter(interviews, village=="God")
 interviews_god<-select(interviews2, no_membrs, years_liv)
-interviews_god<-select(filter(interviews, village=="God'), no_membrs, years live"
+interviews_god<-select(filter(interviews, village=="God')), no_membrs, years live"
                               
 #Pipes
 
 interviews %>%filter(village=="God")%>%select(no_membrs,years_liv)                           
 interviews_god<-interviews %>%filter(village=="God")%>%select(no_membrs,years_liv)
+
+# Pipes
+#exercise
+# Using pipes, subset the interviews data to include interviews where respondents were members of an irrigation association (memb_assoc) and retain only the columns affect_conflicts, liv_count, and no_meals.
+interviews%>%filter(memb_assoc=="yes")%>%select(affect_conflicts, liv_count, no_meals)
+
+#mutate
+
+interviews<-interviews%>%mutate(people_per_room=no_membrs/rooms) #interviews<- used to add this data to this particular table
+
